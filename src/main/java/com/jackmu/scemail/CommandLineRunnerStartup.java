608ca5b1e8d88369c687566ab0849437d90189ef
jackmuva <33766184+jackmuva@users.noreply.github.com>
2024-03-14 16:26:31 -0500

    No longer scheduling jobs; will run email and delete functionality at startup

---
+++ src/main/java/com/jackmu/scemail/CommandLineRunnerStartup.java
@@ -0,0 +1,20 @@
+package com.jackmu.scemail;
+
+
+import com.jackmu.scemail.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerStartup implements CommandLineRunner {
+    @Autowired
+    private EmailService emailService;
+
+    @Override
+    public void run(String...args) throws Exception {
+        emailService.scheduleSendEmails();
+        emailService.deleteFinishedSeries();
+    }
+}
+
